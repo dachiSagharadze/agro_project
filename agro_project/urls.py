ed503e40b9d2f284b37b46ee2d47cf08a3b6d4ff
Dachi Sagharadze <dachi.sagaradze@gmail.com>
2025-10-09 02:20:32 +0400

    fixed media

--- agro_project/urls.py
+++ agro_project/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('news/', include('news.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
